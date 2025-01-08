7ae6f669d368748caee66a13ac568645fcdb4684
evelyny2789 <evelyny2789@nycstudents.net>
2025-01-08 13:24:21 +0000

    AKFNWIEHNFDS

--- src/main/java/com/example/project/Day1.java
+++ src/main/java/com/example/project/Day1.java
@@ -4,6 +4,15 @@ public class Day1{
     private static String[] elf_names = {"Glitter", "Chocolate","Tiny","Snowflake","Frosty"};
 
     public static String generateElfName(String name){
-        return "";  
+        if(name.equals(null) || name.equals("")){
+            throw new IllegalArgumentException("Name cannot be null or empty"); 
+        } else{
+            int stringNum = elf_names.length + 1;
+            int num = (int) Math.random() * (stringNum - 1) + 1; 
+            String elfName = elf_names[num - 1];
+            return elfName + " " + name; 
+        }
+        
     }
+        
 }
